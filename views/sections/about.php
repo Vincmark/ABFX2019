341e517bc820ac1046413ffb1da067281e7c8c23
Vincmark <44532137+Vincmark@users.noreply.github.com>
2019-04-15 23:09:27 +0300

    15apr-2

--- views/sections/about.php
+++ views/sections/about.php
@@ -1,7 +1,8 @@
 <section class="about">
     <div class="about-wrapper">
         <h2 class="about__header header2">About Us</h2>
-        <p class="about__text">We are an experienced team of developers who provide leaders with technical expertise tailored to support their business objectives.</p>
+        <p class="about__text">We are web-development company from Ukraine specializing in complex, back-end oriented projects and beautiful marketing websites.</p>
+        <p class="about__text">Some of our strengths are - dynamic data processing, real-time dashboards making, third party services integrations and convenient interfaces creation.</p>
         <button class="about__learn-more-button button-transparent">Learn more</button>
     </div>
 </section>

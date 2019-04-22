af250e0aafd492980fdcce7556bcae68bc019ac2
Vincmark <44532137+Vincmark@users.noreply.github.com>
2019-04-22 22:52:38 +0300

    22apr

--- views/sections/request-popup.php
+++ views/sections/request-popup.php
@@ -1,25 +1,60 @@
+<!--<section class="request-form-popup" id="id-request-form-popup">-->
+<!--    <div class="request-form-popup-wrapper">-->
+<!--        <button class="request-form-popup__close-cross" id="id-request-form-popup__close-cross"></button>-->
+<!--        <h2 class="request__header header2">Contact us</h2>-->
+<!--        <p class="request__text">Send us a question or a description of your idea and we will prepare an individual offer for you.</p>-->
+<!--        <form class="request_form" action="index.html" method="post" name="request-form" id="id-request-form">-->
+<!--            <div class="">-->
+<!--                <label class="request_form__edit-title" for="id-request-name">Your Name</label>-->
+<!--                <input class="request_form__edit" type="text" value="11" name="name" id="id-request-name">-->
+<!--                <p class="request_form__edit-error-text" id="id-request-name-error">&nbsp;</p>-->
+<!--            </div>-->
+<!--            <div class="">-->
+<!--                <label class="request_form__edit-title" for="id-request-email">Your Email</label>-->
+<!--                <input class="request_form__edit" type="text" value="2@2.2" name="email" id="id-request-email">-->
+<!--                <p class="request_form__edit-error-text" id="id-request-email-error">&nbsp;</p>-->
+<!--            </div>-->
+<!--            <div class="">-->
+<!--                <label class="request_form__edit-title" for="id-request-message">Message</label>-->
+<!--                <textarea class="request_form__textarea" rows="10" name="comment" form="request-form" id="id-request-message">1@1.1</textarea>-->
+<!--                <p class="request_form__textarea-error-text" id="id-request-message-error">&nbsp;</p>-->
+<!--            </div>-->
+<!--            <button class="request_form__send-request-button button-solid" type="submit">Send Request</button>-->
+<!--        </form>-->
+<!--    </div>-->
+<!--</section>-->
+
+
 <section class="request-form-popup" id="id-request-form-popup">
-    <div class="request-form-popup-wrapper">
-        <button class="request-form-popup__close-cross" id="id-request-form-popup__close-cross"></button>
-        <h2 class="request__header header2">Contact us</h2>
-        <p class="request__text">Send us a question or a description of your idea and we will prepare an individual offer for you.</p>
-        <form class="request_form" action="index.html" method="post" name="request-form" id="id-request-form">
-            <div class="">
-                <label class="request_form__edit-title" for="id-request-name">Your Name</label>
-                <input class="request_form__edit" type="text" value="11" name="name" id="id-request-name">
-                <p class="request_form__edit-error-text" id="id-request-name-error">&nbsp;</p>
+    <div class="request-form-popup__overlay">
+        <div class="request-form-popup__overlay-wrapper">
+            <div class="request-form-popup__content-wrapper">
+                <div class="request-form-popup__X3">
+                    <div class="request-form-popup__content">
+                        <button class="request-form-popup__close-cross" id="id-request-form-popup__close-cross"></button>
+                        <h2 class="request__header header2">Contact us</h2>
+                        <p class="request__text">Send us a question or a description of your idea and we will prepare an individual offer for you.</p>
+                        <form class="request_form" action="index.html" method="post" name="request-form" id="id-request-form">
+                            <div class="">
+                                <label class="request_form__edit-title" for="id-request-name">Your Name</label>
+                                <input class="request_form__edit" type="text" value="11" name="name" id="id-request-name">
+                                <p class="request_form__edit-error-text" id="id-request-name-error">&nbsp;</p>
+                            </div>
+                            <div class="">
+                                <label class="request_form__edit-title" for="id-request-email">Your Email</label>
+                                <input class="request_form__edit" type="text" value="2@2.2" name="email" id="id-request-email">
+                                <p class="request_form__edit-error-text" id="id-request-email-error">&nbsp;</p>
+                            </div>
+                            <div class="">
+                                <label class="request_form__edit-title" for="id-request-message">Message</label>
+                                <textarea class="request_form__textarea" rows="10" name="comment" form="request-form" id="id-request-message">1@1.1</textarea>
+                                <p class="request_form__textarea-error-text" id="id-request-message-error">&nbsp;</p>
+                            </div>
+                            <button class="request_form__send-request-button button-solid" type="submit">Send Request</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="">
-                <label class="request_form__edit-title" for="id-request-email">Your Email</label>
-                <input class="request_form__edit" type="text" value="2@2.2" name="email" id="id-request-email">
-                <p class="request_form__edit-error-text" id="id-request-email-error">&nbsp;</p>
-            </div>
-            <div class="">
-                <label class="request_form__edit-title" for="id-request-message">Message</label>
-                <textarea class="request_form__textarea" rows="10" name="comment" form="request-form" id="id-request-message">1@1.1</textarea>
-                <p class="request_form__textarea-error-text" id="id-request-message-error">&nbsp;</p>
-            </div>
-            <button class="request_form__send-request-button button-solid" type="submit">Send Request</button>
-        </form>
+        </div>
     </div>
 </section>
